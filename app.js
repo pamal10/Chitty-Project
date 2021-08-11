1554569b9516e6ca411bc8874fe25ad202be2203
pamal10 <77323645+pamal10@users.noreply.github.com>
2021-08-11 21:20:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret:'Key',cookie:{maxAge:600000}}))
+app.use(session({secret:'Key',cookie:{maxAge:600000},resave:true,saveUninitialized: true}))
 
 db.connect((err)=>{
   if(err) console.log('connection error '+err)
